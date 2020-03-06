fd185f03c2d1b02734ced459038defe08e372e9a
Nate_Sanchez <NSanchez@usa.com>
2020-03-06 12:19:23 -0500

    rendering test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Home from "./Home";
+import { Route, Link, } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>NBA Frontend</h1>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0)
+    console.log("Component did mount")
+  }
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <Link to='/'>
+            <h1>NBA Hub</h1>
+          </Link>
+        </nav>
+        <main>
+          <Route path='/' exact component={Home} />
+        </main>
+        <footer>
+          <div>
+            <h1>This is the footer</h1>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom'
+import './Home.css';
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: null
+        };
+    }
+
+    componentDidMount() {
+        window.scrollTo(0, 0)
+        if (!this.state.data) {
+            fetch("http://localhost:4000/teams")
+                .then(res => res.json())
+                .then(res => {
+                    this.setState({ data: res });
+                    console.log(res)
+                });
+        }
+    }
+
+    render() {
+        if (this.state.data) {
+            let list = this.state.data.map(item => {
+                return (
+                    <div className="eachReturn" key={item.teamId}>
+                        <Link className="imageLink" to={"/team/" + item.name}>
+                            <div>
+                                <img className="teamImages" src={item.logo} alt="team" />
+                            </div>
+                            <p>{item.name}</p>
+                        </Link>
+                    </div>
+                );
+            });
+            return <div className="teamContainer">{list}</div>;
+        } else {
+            return (
+                <div>
+                    <h2>Loading...</h2>
+                </div>
+            )
+        }
+    }
+}
+
+export default Home;
